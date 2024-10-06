78bbd84199f0ed573a346887981f374cba151866
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 02:44:01 +0300

    Update config database

--- src/config/database.php
+++ src/config/database.php
@@ -1,3 +1,21 @@
 <?php
 
-return [];
+return [
+    'driver' => 'mysql',
+    'connection' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => '',
+            'database' => 'test',
+            'charset' => 'utf8',
+        ],
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => 'test',
+            'charset' => 'utf8',
+        ]
+    ]
+];

a0c90967c63a21c29e2befe98f2f8f6acb46eb41
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-06 21:50:18 +0300

    Added redirect for request ended with /

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,12 @@
 use Core\Application;
 use Core\Http\Request;
 
+// Redirect all requests that end with "/"
+if (str_ends_with($_SERVER['REQUEST_URI'], '/')) {
+    header('Location: ' . substr($_SERVER['REQUEST_URI'], 0, -1), true, 301);
+    exit();
+}
+
 // Register Composer
 require __DIR__ . '/../../vendor/autoload.php';
 

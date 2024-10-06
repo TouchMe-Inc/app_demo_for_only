72863bff3488da7a2a0a51da9870997d373346ae
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-06 21:57:36 +0300

    Added model for table users (coming soon)

---
+++ src/app/Models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+class User
+{
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $phone;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+}

55869fbb234b1dbe3e38c52489952f6d0e3ff460
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-06 19:17:31 +0300

    Update variable name ($statusCode -> $status)

--- src/core/Http/Response.php
+++ src/core/Http/Response.php
@@ -6,12 +6,12 @@ class Response
 {
     private array $headers;
     private string $content;
-    private int $statusCode;
+    private int $status;
 
     public function __construct(?string $content = '', int $status = 200, array $headers = [])
     {
         $this->headers = $headers;
         $this->content = $content;
-        $this->statusCode = $status;
+        $this->status = $status;
     }
 }

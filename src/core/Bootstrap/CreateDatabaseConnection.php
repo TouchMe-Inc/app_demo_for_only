c5b2db27ade13441d68a05be3e448486fdc058ef
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 14:54:28 +0300

    Created bootstrapper CreateDatabaseConnection

---
+++ src/core/Bootstrap/CreateDatabaseConnection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core\Bootstrap;
+
+use Core\Application;
+use Core\Bootstrap\Interface\Bootstrapper;
+
+class CreateDatabaseConnection implements Bootstrapper
+{
+
+    public function bootstrap(Application $app): void
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}

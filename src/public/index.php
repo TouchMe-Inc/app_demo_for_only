5fb0b10dabc6b1323b019bc35ab2e5fb0087f938
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 14:26:28 +0300

    Added dummy comments (Visually separate code)

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
@@ -15,12 +15,13 @@ if ($_SERVER['REQUEST_URI'] != "/" && str_ends_with($_SERVER['REQUEST_URI'], '/'
 // Register Composer
 require __DIR__ . '/../../vendor/autoload.php';
 
-// Use magic
-/** @noinspection PhpUnhandledExceptionInspection */
+// Create application
 $app = Application::create(dirname(__DIR__));
 
+//Add routes
 $router = $app->getContainer()->make(Router::class);
 $router->get("/signin", [AuthController::class, "signIn"]);
 $router->get("/", [HomeController::class, "index"]);
 
+// Handle the request
 $app->handleRequest(Request::createFromGlobal());

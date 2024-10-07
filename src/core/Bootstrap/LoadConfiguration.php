96194cf118a1540b63a78a5488628ad531d061d7
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 13:35:41 +0300

    Removed App:loadConfiguration to LoadConfiguration

---
+++ src/core/Bootstrap/LoadConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Bootstrap;
+
+use Core\Application;
+use Core\Bootstrap\Interface\Bootstrapper;
+use Core\Config\Configuration;
+
+class LoadConfiguration implements Bootstrapper
+{
+    public function bootstrap(Application $app): void
+    {
+        $configuration = $app->getContainer()->make(Configuration::class);
+
+        foreach (glob($app->getConfigPath("*.php")) as $filePath) {
+            $configuration->set(basename($filePath, ".php"), require $filePath);
+        }
+    }
+}

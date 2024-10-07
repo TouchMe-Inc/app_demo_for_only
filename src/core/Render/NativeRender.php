c521c63cfe71b808c1a987da9609378566d9b412
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 15:33:54 +0300

    Update DummyRender (now NativeRender)

---
+++ src/core/Render/NativeRender.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Render;
+
+use Core\Render\Interface\Renderer;
+
+class NativeRender implements Renderer
+{
+    public function render(string $template, array $data = []): string
+    {
+        if (!file_exists($template)) {
+            throw new \Exception("Template $template does not exist");
+        }
+
+        extract($data);
+
+        ob_start();
+
+        require $template;
+
+        return ob_get_clean();
+    }
+}

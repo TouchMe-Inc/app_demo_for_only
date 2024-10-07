e9e19b69a65dcd3fe5863503d07ef3f3bd8617cd
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 15:34:17 +0300

    Updated HomeController::index

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Core\Application;
+use Core\Container\Container;
+use Core\Render\NativeRender;
+
 class HomeController
 {
+
     public function index(): string
     {
-        return "index";
+        // TODO: Simplify this
+
+        /** @var Application $app */
+        $app = Container::getInstance()->make(Application::class);
+
+        return (new NativeRender())->render($app->getBasePath() . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . "Views/home.php", ["customString" => "Is my custom string"]);
     }
 }

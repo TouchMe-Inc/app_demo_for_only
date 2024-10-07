da1cbd3467ff2308570e0ee6841e88ac70a8b7bb
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-07 17:41:11 +0300

    Update Exception format

--- src/core/Render/NativeRender.php
+++ src/core/Render/NativeRender.php
@@ -3,13 +3,17 @@
 namespace Core\Render;
 
 use Core\Render\Interface\Renderer;
+use Exception;
 
 class NativeRender implements Renderer
 {
+    /**
+     * @throws Exception
+     */
     public function render(string $template, array $data = []): string
     {
         if (!file_exists($template)) {
-            throw new \Exception("Template $template does not exist");
+            throw new Exception("Template '$template' does not exist.");
         }
 
         extract($data, EXTR_SKIP);

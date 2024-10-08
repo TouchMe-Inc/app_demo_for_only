c62dd6c360d1ee106ecc589961e3762f3141afef
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-08 16:45:36 +0300

    Added apge Users

---
+++ src/app/Views/pages/users.php
@@ -0,0 +1,12 @@
+<?php
+/** @var string $customString */
+?>
+<?php require __DIR__ . '/../components/header.php' ?>
+<main class="flex-grow my-6">
+    <section>
+        <div class="px-6 max-w-6xl mx-auto">
+            <?= "Page: users" ?>
+        </div>
+    </section>
+</main>
+<?php require __DIR__ . '/../components/footer.php' ?>

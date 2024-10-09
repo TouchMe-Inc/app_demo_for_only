2c9f9b7e5027132c0072948eb60c6722837f9b7d
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-09 19:44:45 +0300

    Update database connection 'pgsql'

--- src/config/database.php
+++ src/config/database.php
@@ -12,6 +12,15 @@ return [
             'database' => 'test',
             'charset' => 'utf8',
         ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => 'localhost',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => '',
+            'database' => 'test',
+            'charset' => 'utf8',
+        ],
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => 'test',

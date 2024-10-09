595e8105551e35309f20198b19e078d65f339377
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-09 19:55:33 +0300

    Updated User field id

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 class User
 {
-    private string $id;
+    private int $id;
     private string $name;
     private string $email;
     private string $phone;
 
-    public function getId(): string
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function setId(string $id): void
+    public function setId(int $id): void
     {
         $this->id = $id;
     }

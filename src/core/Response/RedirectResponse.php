96a2902636d0b55df88eb3b73354eea71354896f
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-09 13:07:58 +0300

    Added todo

--- src/core/Response/RedirectResponse.php
+++ src/core/Response/RedirectResponse.php
@@ -6,6 +6,7 @@ class RedirectResponse extends Response
 {
 
     public function __construct(string $url, int $status = 301, array $headers = []) {
+        // TODO: Normalize constructor
         $headers[] = "Location: {$url}";
         parent::__construct('', $status, $headers);
     }

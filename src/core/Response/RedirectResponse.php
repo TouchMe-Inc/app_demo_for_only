cae3976f5a84d6c203f553360edabe1c50ccf8f1
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-09 15:11:26 +0300

    Added response codes as Response Const

--- src/core/Response/RedirectResponse.php
+++ src/core/Response/RedirectResponse.php
@@ -5,7 +5,7 @@ namespace Core\Response;
 class RedirectResponse extends Response
 {
 
-    public function __construct(string $url, int $status = 301, array $headers = []) {
+    public function __construct(string $url, int $status = self::MOVED_PERMANENTLY, array $headers = []) {
         // TODO: Normalize constructor
         $headers[] = "Location: {$url}";
         parent::__construct('', $status, $headers);

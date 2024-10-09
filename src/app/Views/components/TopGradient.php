5911ef4874d3e7e54d88871180f68e82517437e6
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-09 20:29:34 +0300

    Added TogGradient component for view

---
+++ src/app/Views/components/TopGradient.php
@@ -0,0 +1,6 @@
+<div class="relative pointer-events-none" aria-hidden="true">
+    <div class="absolute flex flex-wrap justify-center content-center top-0 left-1/2 w-1/2 max-w-4xl aspect-square -translate-x-1/2 -translate-y-1/2 -z-10">
+        <div class="absolute rounded-full bg-violet-500 dark:bg-violet-500 opacity-10 dark:opacity-20 blur-[120px] inset-0"></div>
+        <div class="absolute rounded-full bg-violet-400 dark:bg-violet-400 opacity-50 dark:opacity-75 blur-[30px] dark:blur-[80px] w-64 h-64 bottom-1/2"></div>
+    </div>
+</div>

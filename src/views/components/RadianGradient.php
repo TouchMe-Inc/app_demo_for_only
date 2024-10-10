315913d0ed7a7567377cdeee2100db8779263593
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 18:44:43 +0300

    Added component RadianGradient.php

---
+++ src/views/components/RadianGradient.php
@@ -0,0 +1,3 @@
+<div aria-hidden="true" class="absolute bottom-0 left-1/2 w-1/3 aspect-square -translate-x-1/2 translate-y-1/2 -z-10">
+    <div class="absolute rounded-full bg-violet-500/20 dark:bg-violet-500/10 blur-[80px] inset-0"></div>
+</div>

8ba8ca8b698ce68565d2f76094fb2422523f4eca
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 18:45:44 +0300

    Implement bootstrap CreateDatabaseConnection

--- src/app/Bootstrap/CreateDatabaseConnection.php
+++ src/app/Bootstrap/CreateDatabaseConnection.php
@@ -4,12 +4,26 @@ namespace App\Bootstrap;
 
 use Core\Application;
 use Core\Bootstrap\Interface\Bootstrapper;
+use Core\Database\ConnectionFactory;
+use Core\Database\Interface\Connection;
 
 class CreateDatabaseConnection implements Bootstrapper
 {
 
     public function bootstrap(Application $app): void
     {
-        // TODO: Implement bootstrap() method.
+        $configuration = $app->configuration()->get("database");
+
+        $driver = $configuration["driver"];
+        $options = $configuration['connection'][$driver];
+
+        $connection = $app->container()
+            ->make(ConnectionFactory::class)
+            ->createConnection($driver, $options);
+
+        $app->container()->addInstance(
+            Connection::class,
+            $connection
+        );
     }
 }

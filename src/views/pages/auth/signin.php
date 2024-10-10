9d503b7145d407ff360a8d01ec284c442fce69d4
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 19:56:33 +0300

    Updated design signIn and signUp

--- src/views/pages/auth/signin.php
+++ src/views/pages/auth/signin.php
@@ -6,26 +6,35 @@ use App\Views\View;
 <main class="flex-grow my-6">
     <section>
         <div class="px-6 max-w-6xl mx-auto">
-            <div class="text-center pb-8">
-                <h1 class="text-4xl">Sign in your account</h1>
-            </div>
-            <div class="mx-auto max-w-xs">
-                <form method="post">
-                    <div class="mb-4">
-                        <label for="name_or_email" class="block mb-1 text-sm ">Name or Email <span class="text-red-500">*</span></label>
-                        <input id="name_or_email" type="text" required autofocus
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+            <div class="pt-24 pb-10">
+                <div class="text-center pb-8">
+                    <h1 class="text-4xl">Sign in your account</h1>
+                </div>
+                <div class="mx-auto max-w-xs">
+                    <form method="post">
+                        <div class="mb-4">
+                            <label for="name_or_email" class="block mb-1 text-sm ">Name or Email <span
+                                        class="text-red-500">*</span></label>
+                            <input id="name_or_email" type="text" required autofocus
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div class="mb-8">
+                            <label for="password" class="block mb-1 text-sm">Password <span
+                                        class="text-red-500">*</span></label>
+                            <input id="password" type="password" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div>
+                            <button class="w-full px-3 py-2 rounded-lg bg-purple-600" type="submit">Sign In</button>
+                        </div>
+                    </form>
+                    <div class="pt-3">
+                        <div class="text-gray-400">
+                            Don't have an account? <a href="/signup"
+                                                      class="hover:text-black text-gray-700 transition-colors duration-300 dark:text-gray-300 dark:hover:text-white">Sign Up</a>
+                        </div>
                     </div>
-                    <div class="mb-8">
-                        <label for="password" class="block mb-1 text-sm">Password <span
-                                    class="text-red-500">*</span></label>
-                        <input id="password" type="password" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
-                    </div>
-                    <div>
-                        <button class="w-full px-3 py-2 rounded-lg bg-purple-600" type="submit">Sign In</button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     </section>
--- src/views/pages/auth/signup.php
+++ src/views/pages/auth/signup.php
@@ -6,42 +6,54 @@ use App\Views\View;
 <main class="flex-grow my-6">
     <section>
         <div class="px-6 max-w-6xl mx-auto">
-            <div class="text-center">
-                <h1 class="text-3xl">Create your account</h1>
-            </div>
-            <div class="mx-auto max-w-xs">
-                <form method="post">
-                    <div class="mb-4">
-                        <label for="name" class="block mb-1 text-sm ">Name <span class="text-red-500">*</span></label>
-                        <input id="name" type="text" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+            <div class="pt-24 pb-10">
+                <div class="text-center pb-8">
+                    <h1 class="text-4xl">Create your account</h1>
+                </div>
+                <div class="mx-auto max-w-xs">
+                    <form method="post">
+                        <div class="mb-4">
+                            <label for="name" class="block mb-1 text-sm ">Name <span
+                                        class="text-red-500">*</span></label>
+                            <input id="name" type="text" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div class="mb-4">
+                            <label for="email" class="block mb-1 text-sm">Email <span
+                                        class="text-red-500">*</span></label>
+                            <input id="email" type="email" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div class="mb-4">
+                            <label for="phone" class="block mb-1 text-sm">Phone <span
+                                        class="text-red-500">*</span></label>
+                            <input id="phone" type="tel" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div class="mb-4">
+                            <label for="password" class="block mb-1 text-sm">Password <span
+                                        class="text-red-500">*</span></label>
+                            <input id="password" type="password" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div class="mb-8">
+                            <label for="repassword" class="block mb-1 text-sm">Re-password <span
+                                        class="text-red-500">*</span></label>
+                            <input id="repassword" type="password" required
+                                   class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
+                        </div>
+                        <div>
+                            <button class="w-full px-3 py-2 rounded-lg bg-purple-600" type="submit">Sign Up</button>
+                        </div>
+                    </form>
+                    <div class="pt-3">
+                        <div class="text-gray-400">
+                            Already have an account? <a href="/signin"
+                                                        class="hover:text-black text-gray-700 transition-colors duration-300 dark:text-gray-300 dark:hover:text-white">Sign
+                                In</a>
+                        </div>
                     </div>
-                    <div class="mb-4">
-                        <label for="email" class="block mb-1 text-sm">Email <span class="text-red-500">*</span></label>
-                        <input id="email" type="email" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
-                    </div>
-                    <div class="mb-4">
-                        <label for="phone" class="block mb-1 text-sm">Phone <span class="text-red-500">*</span></label>
-                        <input id="phone" type="tel" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
-                    </div>
-                    <div class="mb-4">
-                        <label for="password" class="block mb-1 text-sm">Password <span
-                                    class="text-red-500">*</span></label>
-                        <input id="password" type="password" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
-                    </div>
-                    <div class="mb-8">
-                        <label for="repassword" class="block mb-1 text-sm">Re-password <span
-                                    class="text-red-500">*</span></label>
-                        <input id="repassword" type="password" required
-                               class="w-full text-sm px-3 py-2 bg-gray-800 rounded-lg border border-transparent focus:border-purple-600 focus:outline-none"/>
-                    </div>
-                    <div>
-                        <button class="w-full px-3 py-2 rounded-lg bg-purple-600" type="submit">Sign Up</button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     </section>

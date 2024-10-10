42c588c23137efeb94d29918a6f1c04e2edb3c23
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 14:08:30 +0300

    Removed creation of a new renderer for each render

--- src/app/Views/View.php
+++ src/app/Views/View.php
@@ -2,15 +2,18 @@
 
 namespace App\Views;
 
+use Core\Render\Interface\Renderer;
 use Core\Render\NativeRender;
 
 class View
 {
 
+    private static Renderer|null $renderer = null;
+
     public static function render(string $view, array $data = []): string
     {
-        return (new NativeRender())->render(
-            app()->getBasePath() . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $view . ".php",
+        return self::getRenderer()->render(
+            basepath() . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $view . ".php",
             $data
         );
     }
@@ -28,5 +31,14 @@ class View
     public static function component(string $view, array $data = []): string
     {
         return self::render('components' . DIRECTORY_SEPARATOR . $view, $data);
+    }
+
+    private static function getRenderer(): Renderer
+    {
+        if (is_null(self::$renderer)) {
+            self::$renderer = new NativeRender();
+        }
+
+        return self::$renderer;
     }
 }

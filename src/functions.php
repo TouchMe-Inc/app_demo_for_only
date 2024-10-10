54b569812fdac7170e573ba4273decf429a3800a
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 14:03:02 +0300

    Added function to get container and base path

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,25 @@
 <?php
 
 use Core\Application;
+use Core\Container\Container;
 
 if (!function_exists('app')) {
-    function app()
+    function app(): Application
     {
         return Application::getInstance();
+    }
+}
+
+if (!function_exists('container')) {
+    function container(): Container
+    {
+        return app()->container();
+    }
+}
+
+if (!function_exists('basepath')) {
+    function basepath(): string
+    {
+        return app()->getBasePath();
     }
 }

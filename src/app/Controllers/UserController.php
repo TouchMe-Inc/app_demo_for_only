15f76ca9b0f17ad7a88902f20bf27e99cfea7ea5
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-10 18:43:08 +0300

    Added UserService to UserController

--- src/app/Controllers/UserController.php
+++ src/app/Controllers/UserController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controllers;
 
+use App\Services\UserService;
 use App\Views\View;
 
 class UserController
 {
+    private UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function index(): string
     {
         return View::layout("base", [
             "slot" => View::page("users")
+            "slot" => View::page("users/index", ['users' => $this->userService->all()])
+        ]);
+    }
         ]);
     }
 }

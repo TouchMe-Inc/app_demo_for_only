f88d1e47ea130e8755e46d16d98c2f1e4368de2b
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-11 20:45:27 +0300

    Added field password for model User

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -9,6 +9,8 @@ class User
     private string $email;
     private string $phone;
 
+    private string $password;
+
     public function getId(): int
     {
         return $this->id;
@@ -47,5 +49,15 @@ class User
     public function setPhone(string $phone): void
     {
         $this->phone = $phone;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
     }
 }

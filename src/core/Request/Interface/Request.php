cc76fb27217dcec4ca91799a553742bab5dfce2a
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-16 20:15:51 +0300

    Update Request Interface Added get/has/setSession

--- src/core/Request/Interface/Request.php
+++ src/core/Request/Interface/Request.php
@@ -2,11 +2,44 @@
 
 namespace Core\Request\Interface;
 
+use Core\Session\interface\Session as SessionInterface;
+
 interface Request
 {
+    /**
+     * @return static
+     */
     public static function createFromGlobal(): static;
 
+    /**
+     * @return string
+     */
     public function getMethod(): string;
 
+    /**
+     * @return string
+     */
     public function getUri(): string;
+
+    /**
+     * Get the Session.
+     *
+     * @return SessionInterface
+     */
+    public function getSession(): SessionInterface;
+
+    /**
+     * Set the Session.
+     *
+     * @param SessionInterface $session
+     * @return void
+     */
+    public function setSession(SessionInterface $session): void;
+
+    /**
+     * Whether the request contains a Session.
+     *
+     * @return bool
+     */
+    public function hasSession(): bool;
 }

878a21eb7d468913a4a9866e5b9899071e735ba5
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-10-16 18:14:21 +0300

    Added comment for interface Rule

--- src/core/Validation/Interface/Rule.php
+++ src/core/Validation/Interface/Rule.php
@@ -4,5 +4,11 @@ namespace Core\Validation\Interface;
 
 interface Rule
 {
+    /**
+     * Run the validation rule.
+     *
+     * @param mixed $value
+     * @return bool
+     */
     public function validate(mixed $value): bool;
 }
